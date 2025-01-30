84c4e1c5483453b67f47269df8a918210d2e8339
richard <richiebbaah@gmail.com>
2025-01-29 19:16:58 -0500

    set up basics of application , deploying early to aws

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("hello world")
-print("richard")
-print("tyler")
-print("tjb")
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def helloworld():
+    return "Hello World!"
+if __name__ == "__main__":
+    app.run(port=8080)
