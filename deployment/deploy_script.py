f3c3a40db2cc19301e8681e583bbeb1557bfbbe2
Tyler Smith <145503664+Tylerrs1423@users.noreply.github.com>
2025-02-10 11:41:50 -0500

    add helper function to simplify deployment

--- deployment/deploy_script.py
+++ deployment/deploy_script.py
@@ -54,10 +54,12 @@ class EC2_Deployment:
 
     
     def create_key_pair(self):
+        #You can change the name to whatever you want
         key_name = f"MyUniqueKeyPair-{uuid.uuid4().hex[:8]}"
         self.key_wrapper.create(key_name)
         return key_name
-    
+
+        #You can assign group name
     def create_security_group(self, vpc_id):
         group_name = f"MySecurityGroup-{uuid.uuid4().hex[:8]}"
         description = 'Description of your group!!!'
@@ -68,6 +70,16 @@ class EC2_Deployment:
     def launch_instance(self, ami_id, instance_type, key_name, security_group_id):
         return self.inst_wrapper.launch_instance(ami_id, instance_type, key_name, security_group_id)
 
+    def save_resource_ids(self, instance_id, key_name, sg_id, allocation_id):
+        resource_ids = {
+            "instance_id": instance_id,
+            "key_name": key_name,
+            "sg_id": sg_id,
+            "allocation_id": allocation_id
+        }
+        with open("resource_ids.txt", "w") as file:
+            json.dump(resource_ids, file)
+        console.print("Resource IDs saved to resource_ids.txt")
 
     def associate_elastic_ip(self, instance_id):
         elastic_ip = self.eip_wrapper.allocate()
@@ -82,6 +94,7 @@ class EC2_Deployment:
         elif action == 'reboot':
             self.inst_wrapper.reboot_instance(instance_id)
 
+    
     def cleanup_resources(self, instance_id, key_name, sg_id, allocation_id):
         self.inst_wrapper.terminate_instance(instance_id)
         self.key_wrapper.delete(key_name)
@@ -89,69 +102,44 @@ class EC2_Deployment:
         self.eip_wrapper.release(allocation_id)
         if self.allocation_id:
             self.eip_wrapper.release(self.allocation_id)
-    
-if __name__ == "__main__":
-    '''
-    os.environ["AWS_PROFILE"] = "AdministratorAccess-746669232870"  
-    region_name = 'us-east-1' 
 
-    ec2_client = boto3.client('ec2', region_name=region_name)
-    ssm_client = boto3.client('ssm', region_name=region_name)
-
-    # Initialize wrappers
-    inst_wrapper = EC2InstanceWrapper(ec2_client)
-    key_wrapper = KeyPairWrapper(ec2_client)
-    sg_wrapper = SecurityGroupWrapper(ec2_client)
-    eip_wrapper = ElasticIpWrapper(ec2_client)
-
-    # Initialize deployment
-    deployment = EC2_Deployment(inst_wrapper, key_wrapper, sg_wrapper, eip_wrapper, ssm_client)
-
-    # Replace these with the actual resource IDs you created in the last run
-    instance_id = 'i-07fe51071b3af9e11'  # Use the instance ID from your output
-    key_name = 'MyUniqueKeyPair-fc1a82a3'  # Use the key pair name from your output
-    sg_id = 'sg-0412c50e1554f701e'  # Use the security group ID from your output
-    allocation_id = None  # Add the allocation ID if you have allocated an Elastic IP
-
-    # Perform cleanup
-    deployment.cleanup_resources(instance_id, key_name, sg_id, allocation_id)
-    console.print("Resources cleaned up")
-    '''
-
-    
-    region_name = 'us-east-1' 
-    os.environ["AWS_PROFILE"] = "AdministratorAccess-746669232870"  
-
-
-    ec2_client = boto3.client('ec2', region_name=region_name)
-    ssm_client = boto3.client('ssm', region_name=region_name)
-
-    inst_wrapper = EC2InstanceWrapper(ec2_client)
-    key_wrapper = KeyPairWrapper(ec2_client)
-    sg_wrapper = SecurityGroupWrapper(ec2_client)
-    eip_wrapper = ElasticIpWrapper(ec2_client)
-
-    deployment = EC2_Deployment(inst_wrapper,key_wrapper,sg_wrapper,eip_wrapper,ssm_client)
-
+def setup_deployment():
+    os.environ["AWS_PROFILE"] = "AdministratorAccess-746669232870"
+    region_name = 'us-east-1'
     vpc_id = 'vpc-08293ca8a1ffcef5c'
 
-    
+    ec2_client = boto3.client('ec2', region_name=region_name)
+    ssm_client = boto3.client('ssm', region_name=region_name)
+
+    inst_wrapper = EC2InstanceWrapper(ec2_client)
+    key_wrapper = KeyPairWrapper(ec2_client)
+    sg_wrapper = SecurityGroupWrapper(ec2_client)
+    eip_wrapper = ElasticIpWrapper(ec2_client)
+
+    deployment = EC2_Deployment(ec2_client, inst_wrapper, key_wrapper, sg_wrapper, eip_wrapper, ssm_client)
+
     key_name = deployment.create_key_pair()
     console.print(f"Key pair created: {key_name}")
 
     sg_id = deployment.create_security_group(vpc_id)
     console.print(f"Security group created: {sg_id}")
 
-
-    ami_id = 'ami-04681163a08179f28'  # AMI, use whatever one from catalog
+    ami_id = 'ami-04681163a08179f28'  # Use an appropriate AMI ID from your AWS account
     instance_type = 't2.micro'
     instance_id = deployment.launch_instance(ami_id, instance_type, key_name, sg_id)
     console.print(f"Instance launched: {instance_id}")
 
+    allocation_id = None  # Replace with your actual allocation ID if you have one
 
-    allocation_id = None  # Add the allocation ID if you have allocated an Elastic IP
-    deployment.cleanup_resources(instance_id, key_name, sg_id, allocation_id)
-    console.print("Resources cleaned up")
+    deployment.save_resource_ids(instance_id, key_name, sg_id, allocation_id)
+
+    return deployment
+    
+if __name__ == "__main__":
+    deployment = setup_deployment
+    deployment.cleanup_resources()
+
+    
 
     
 
