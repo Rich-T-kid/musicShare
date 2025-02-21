a5200ef05323b8c0312ed425197427bdda01ad3d
Edson Petry <edsonlbpetry@gmail.com>
2025-02-21 13:20:01 -0500

    checkpoint: just imported react router

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,6 @@
+
+const HomePage = () => {
+    <div>
+        This will be a homepage one day I promse
+    </div>
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,11 @@
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import { BrowserRouter, Routes, Route } from "react-router"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
+    </BrowserRouter>
+    )
